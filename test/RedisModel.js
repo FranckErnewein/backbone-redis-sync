9d131c2709157a0af246bf0756d8e36d0cabc17c
Franck Ernewein <franck.ernewein@gmail.com>
2014-07-11 19:54:31 +0200

    sync: update

---
+++ test/RedisModel.js
@@ -0,0 +1,7 @@
+var RedisModel = require('..').RedisModel;
+var client = require('redis').createClient();
+
+describe('RedisModel', function() {
+
+
+});
--- test/sync.js
+++ test/sync.js
@@ -1,6 +1,39 @@
 var Backbone = require('backbone');
 var sync = require('..').sync;
+var expect = require('chai').expect;
+var client = require('redis').createClient();
 
 Backbone.sync = sync;
 
-describe('sync', function() {});
+describe('sync', function() {
+
+  describe('create', function() {
+
+    var data = {
+      id: 'test_save',
+      test: 'some data'
+    };
+    /*
+    beforeEach(function(done){
+      redis.del(data.id, function(){
+        done();  
+      });
+    });
+    */
+    before(function(done) {
+      client.del(data.id, function() {
+        done();
+      });
+    });
+    it('should save in redis', function(done) {
+      var model = new Backbone.Model(data);
+      model.save().then(function() {
+        client.hget(data.id, 'test', function(err, fromRedis) {
+          expect(data.test).to.deep.equal(fromRedis);
+          done();
+        });
+      });
+    });
+  });
+
+});
